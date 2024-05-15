54af0ea91dc60c1a7f308fa03799341d7de85b16
Alim Karaca <alim.karaca2000@gmail.com>
2024-05-15 14:57:59 +0300

    Mobile Responsivity v0.13

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useRef, useEffect } from "react"
 import "../App.css"
 import Logo from "../assets/logo.png"
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"
@@ -7,10 +7,25 @@ import HamburgerMenu from "./HamburgerMenu"
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const menuRef = useRef(null)
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen)
   }
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setIsMenuOpen(false)
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside)
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [])
 
   return (
     <div className='header'>
@@ -24,9 +39,13 @@ const Navbar = () => {
         <Link to='/request'>
           <button className='navbtn home'>request</button>
         </Link>
-        <IconMenu2 className='hamburger' stroke={2} size={40} onClick={toggleMenu} />
+        <IconMenu2 className={`hamburger ${isMenuOpen ? "white" : ""}`} stroke={2} size={40} onClick={toggleMenu} />
       </div>
-      {isMenuOpen && <HamburgerMenu />}
+      {isMenuOpen && (
+        <div ref={menuRef}>
+          <HamburgerMenu />
+        </div>
+      )}
     </div>
   )
 }
