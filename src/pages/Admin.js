b88cd165907d29eb1cdab7ab5d83e160a0709aee
Alim Karaca <alim.karaca2000@gmail.com>
2024-05-20 10:35:42 +0300

    PostgreSQL Integration v0.3

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -3,22 +3,21 @@ import { useEffect, useState } from "react"
 
 const Admin = () => {
   const [data, setData] = useState([])
-  // ...
+  const connectionString = "postgres://default:4atdeI9FWLwp@ep-lucky-sky-a23xax34.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require"
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const { rows } = await sql`
-        SELECT id, mediaorder, media
-        FROM watch_order
-      `
+          SELECT *
+          FROM watch_order
+        `({ connectionString })
         setData(rows)
         console.log(data)
       } catch (error) {
         console.error("Error fetching data:", error)
       }
     }
-
     fetchData()
   }, [])
 
