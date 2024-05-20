3a0574b067054639c9a89278292d74c2e1f64960
Alim Karaca <alim.karaca2000@gmail.com>
2024-05-20 11:55:48 +0300

    Small Changes

--- src/components/BACKUP/Footer.js
+++
@@ -1,5 +0,0 @@
-const Footer = () => {
-  return <div className='footer'>What Order 2024 ©</div>
-}
-
-export default Footer
--- src/data/WatchOrderPostgreSQL.js
+++
@@ -1,8 +0,0 @@
-import { sql } from "@vercel/postgres"
-
-export default async function WatchOrderPOST({ params }) {
-  const { rows } = await sql`SELECT * from CARTS where user_id=${params.watch_order}`
-  console.log(rows)
-
-  return <div></div>
-}
